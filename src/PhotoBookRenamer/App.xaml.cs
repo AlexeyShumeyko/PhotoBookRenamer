6f22921cb63f2ac2928cef4494b74d355bc30bc4
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2025-11-29 21:50:20 +0300

    initialize WPF application structure

---
+++ src/PhotoBookRenamer/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace PhotoBookRenamer
+{
+    public partial class App : Application
+    { 
+    }
+}
---
+++ src/PhotoBookRenamer/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using PhotoBookRenamer.ViewModels;
+using System.Windows;
+
+namespace PhotoBookRenamer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
